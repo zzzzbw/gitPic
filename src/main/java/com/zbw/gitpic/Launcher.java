09127883bb0a3a70712af2454920d5ecb6be89b8
zzzzbw <zzzzbw@gmail.com>
2019-01-29 17:05:14 +0800

    feat(pom.xml): 升级为java11版本

---
+++ src/main/java/com/zbw/gitpic/Launcher.java
@@ -0,0 +1,8 @@
+package com.zbw.gitpic;
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        Bootstrap.main(args);
+    }
+}
--- src/main/java/com/zbw/gitpic/controller/MainController.java
+++ src/main/java/com/zbw/gitpic/controller/MainController.java
@@ -8,7 +8,6 @@ import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
